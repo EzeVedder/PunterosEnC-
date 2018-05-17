dc37030c1fccd035d2dfdad27f3d9e3d38d9dd06
EzeVedder <pane.ezequiel@gmail.com>
2018-05-17 12:19:33 -0300

    CadenaCaracteres_v2

--- CadenaCaracteres/main.c
+++ CadenaCaracteres/main.c
@@ -3,6 +3,28 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    char nombre[11] = {"MiNombre"};
+    char *apellido = {"MiApellido"};
+    char *puntero;
+    puntero = nombre;
+
+    printf("\n%s",nombre);
+    printf("\n%s",apellido);//--->no lo muestra porque en realidad es un puntero (int)
+    //printf("\n%s",*puntero);
+    printf("\n%s",puntero);
+
+    int i;
+    for(i=0;i<11;i++)
+    {
+        if(*(apellido+i)!= '\0')
+        {
+            printf("\n%c",*(apellido+i));
+        }
+    }
+
+    printf("\n----------");
+    printf("\n%s",apellido);
+
+
     return 0;
 }
