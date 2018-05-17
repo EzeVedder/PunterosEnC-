6be628d73f91b7a8c9fd323250e4c0759a862c49
EzeVedder <pane.ezequiel@gmail.com>
2018-05-17 11:55:30 -0300

    VectoresConPunteros_Desplazamiento

--- Vectores/main.c
+++ Vectores/main.c
@@ -7,11 +7,24 @@ int main()
 {
     int numeros[3];
     int *puntero;//es una direccion de memoria y tambien tiene un tipo de dato
+    puntero = numeros;
+    int auxiliar = 333;
 
     numeros[0] = 9;
     numeros[1] = 999;
     numeros[2] = 9999;
+    printf("\nnumero :%d",*puntero);
+    printf("\nnumero :%d",*(puntero+1));
+    printf("\nnumero :%d",*(puntero+2));
+    printf("\nnumero :%d",*(puntero+3));
+    printf("\nnumero :%d",*(puntero+4));
+
+    printf("\n-------------------");
+    printf("\nnumero :%d",*numeros);
+
     mostrar(numeros,3);
+
+
 
     return 0;
 }
