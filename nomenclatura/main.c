321c92635519d42c34801218604abfb91ce057de
EzeVedder <pane.ezequiel@gmail.com>
2018-05-17 10:24:51 -0300

    nomenclatura2

--- nomenclatura/main.c
+++ nomenclatura/main.c
@@ -3,6 +3,29 @@
 
 int main()
 {
-    printf("Hello world!\n");
+
+    //punteros
+    int *punteroNumero;
+    int numero;
+    int numeroDos;
+    numero = 66;
+    //&numeroDos = &numero;
+    punteroNumero = &numero;//le asigno al puntero(en direcccion en memoria) el valor de la variable
+
+    numeroDos = &numero;// asignacion de memoria
+    printf("a- %d\n",numeroDos);
+
+    numeroDos = *punteroNumero; //asignacion de valor
+    printf("b- %d\n",numeroDos);
+    //*punteroNumero = &numero;
+    //*punteroNumero = 55;
+    printf("%d\n",numero);
+    printf("%d\n",numeroDos);
+    printf("%d\n",&numero);
+    printf("%p\n",punteroNumero);//posicion de memoria de variable
+    printf("%p\n",&punteroNumero);//posicion de memoria del puntero
+    printf("%d\n",*punteroNumero);//valor de la posicion de memoria
+    //printf("%d",*punteroNumero);
+
     return 0;
 }
