1f39ab4ba1f278ab245858f63d8a147a2ecb9d48
unknown <pane.ezequiel@gmail.com>
2018-05-18 11:54:12 -0300

    Archivos

---
+++ Archivos/Archivos_en_C/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    /*FILE *fp;
+
+    char buffer[100] = "Esto es un texto dentro del fichero.";
+    char bufferIn[100]
+
+    fp = fopen( "D:\\clase\\fichero.txt","w" );
+
+    fprintf(fp,buffer);
+
+    fclose( fp );
+    system("pause");
+    system("cls");*/
+
+    return 0;
+}
